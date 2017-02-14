7417bbeff72c286d10cd8e6bd3005d34655d62e8
Benjamin Efron <Benjamin Efron>
2017-02-14 00:55:48 -0500

    drawing and then dragging works, dragging, drawing, then dragging again does NOT work

--- linerider/src/linerider/gui.clj
+++ linerider/src/linerider/gui.clj
@@ -23,11 +23,11 @@
 
 (defn newLine [x1 y1 x2 y2]
   {:p1 [x1 y1]
-   :p2 [x2 y2]
-   :swingObj (new java.awt.geom.Line2D$Double x1 y1 x2 y2)})
+   :p2 [x2 y2]})
 
 (defn newState []
-  {:mode :none
+  {:mode :line
+   :offset [0 0]
    :lines (list)})
 
 ;Timer that sends update signals to the frame to repaint
@@ -38,40 +38,90 @@
        (.repaint world)
    )))
 
-(defn drawDragListener [drawingState worldState]
+(defn handleDrawLineDrag [e drawingState offset]
+  (let [[offX offY] offset
+        [baseSX baseSY] (@drawingState :p1)
+        mX (+ (.getX e) offX)
+        mY (+ (.getY e) offY)
+        sX (+ baseSX offX)
+        sY (+ baseSY offY)]
+    (dosync
+      (ref-set drawingState (assoc @drawingState :p2 [mX mY])))))
+
+(defn handleMoveDrag [e dragState worldState]
+  (let [[cOffX cOffY] (@worldState :offset)
+        newOffX (+ (.getX e) cOffX)
+        newOffY (+ (.getY e) cOffY)]
+    (dosync (ref-set dragState (assoc @dragState :p2 [newOffX newOffY])))))
+
+
+(defn dragListener [drawingState dragState worldState]
   (proxy [MouseMotionListener] []
 
     (mouseDragged [e]
       (if (= (@worldState :mode) :line)
-        (let [mX (.getX e)
-              mY (.getY e)
-              [sX sY] (@drawingState :p1)]
-          (dosync
-            (ref-set drawingState (assoc @drawingState :swingObj (new java.awt.geom.Line2D$Double sX sY mX mY)))))))
+        (handleDrawLineDrag e drawingState (@worldState :offset))
+        (handleMoveDrag e dragState worldState)))
 
     (mouseMoved [e])))
-
 
 (defn drawListener [drawingState worldState]
   (proxy [MouseListener] []
 
     (mousePressed [e]
       (if (= (@worldState :mode) :line)
-        (let [x (.getX e)
-              y (.getY e)]
+        (let [[offX offY] (@worldState :offset)
+              x (+ (.getX e) offX)
+              y (+ (.getY e) offY)]
           (dosync
             (ref-set drawingState (assoc @drawingState :currentlyDrawing true))
             (ref-set drawingState (assoc @drawingState :p1 [x y]))
-            (ref-set drawingState (assoc @drawingState :swingObj (new java.awt.geom.Line2D$Double x y (+ x 1) (+ y 1))))))))
+            (ref-set drawingState (assoc @drawingState :p2 [(+ x 1) (+ y 1)]))))))
 
     (mouseReleased [e]
       (if (= (@worldState :mode) :line)
-        (let [[startX startY] (@drawingState :p1)
-              endX (.getX e)
-              endY (.getY e)]
+        (let [[offX offY] (@worldState :offset)
+              [baseSX baseSY] (@drawingState :p1)
+              startX (+ baseSX offX)
+              startY (+ baseSY offY)
+              endX (+ (.getX e) offX)
+              endY (+ (.getY e) offY)]
           (dosync
             (ref-set drawingState (assoc @drawingState :currentlyDrawing false))
+            (ref-set drawingState (assoc @drawingState :p2 [endX endY]))
             (ref-set worldState (assoc @worldState :lines (cons (newLine startX startY endX endY) (@worldState :lines))))))))
+
+    (mouseEntered [e])
+
+    (mouseExited [e])
+
+    (mouseClicked [e])))
+
+(defn dragClickListener [dragState worldState]
+  (proxy [MouseListener] []
+
+    (mousePressed [e]
+      (if (= (@worldState :mode) :drag)
+        (let [[offX offY] (@worldState :offset)
+              x (+ (.getX e) offX)
+              y (+ (.getY e) offY)]
+          (dosync
+            (ref-set dragState (assoc @dragState :currentlyDragging true))
+            (ref-set dragState (assoc @dragState :p1 [x y]))
+            (ref-set dragState (assoc @dragState :p2 [x y]))))))
+
+    (mouseReleased [e]
+      (if (= (@worldState :mode) :drag)
+        (let [[baseOffX baseOffY] (@worldState :offset)
+              [sX sY] (@dragState :p1)
+              [eX eY] (@dragState :p2)
+              newXDif (- eX sX)
+              newYDif (- eY sY)]
+          (dosync
+            (ref-set dragState (assoc @dragState :currentlyDragging false))
+            (ref-set dragState (assoc @dragState :p1 [0 0]))
+            (ref-set dragState (assoc @dragState :p2 [0 0]))
+            (ref-set worldState (assoc @worldState :offset [(+ baseOffX newXDif) (+ baseOffY newYDif)]))))))
 
     (mouseEntered [e])
 
@@ -93,13 +143,21 @@
     (actionPerformed [e]
       (dosync (ref-set state (assoc @state :mode mode))))))
 
-(defn paint [g line]
-  (let [toDraw (line :swingObj)]
+(defn paint [g line offset dragState]
+  (let [[offX offY] offset
+        [sCOffX sCOffY] (dragState :p1)
+        [eCOffX eCOffY] (dragState :p2)
+        cOffX (- eCOffX sCOffX)
+        cOffY (- eCOffY sCOffY)
+        fullOffX (+ offX cOffX)
+        fullOffY (+ offY cOffY)
+        [sX sY] (line :p1)
+        [eX eY] (line :p2)
+        toDraw (new java.awt.geom.Line2D$Double (+ sX  fullOffX) (+ sY fullOffY) (+ eX fullOffX) (+ eY fullOffY))]
     (.setColor g Color/black)
     (.setStroke g (BasicStroke. 4))
     (.fill g toDraw)
     (.draw g toDraw)))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;
 ;GUI
@@ -129,6 +187,11 @@
           :p1 nil
           :swingObj nil}))
 
+  (def dragState
+    (ref {:currentlyDragging false
+          :p1 [0 0]
+          :p2 [0 0]}))
+
   (let [p (proxy [JPanel] []
             (getPreferredSize []
               WORLDBOUNDS)
@@ -137,17 +200,18 @@
               (proxy-super paintComponent g)
 
               (if (@drawingState :currentlyDrawing)
-                    (paint g @drawingState))
+                    (paint g @drawingState (@state :offset) @dragState))
 
               (loop [lines (@state :lines)]
                   (if (empty? lines)
                     :done
                     (do
-                      (paint g (first lines))
+                      (paint g (first lines) (@state :offset) @dragState)
                       (recur (rest lines)))))))]
 
     (.addMouseListener p (drawListener drawingState state))
-    (.addMouseMotionListener p (drawDragListener drawingState state))
+    (.addMouseListener p (dragClickListener dragState state))
+    (.addMouseMotionListener p (dragListener drawingState dragState state))
     (.setLayout p (FlowLayout.))
     (.setBackground p Color/white)
     p))
@@ -171,7 +235,7 @@
   ;add buttons to the control panel
   (.add controls (createButton "play" (doNothing)))
   (.add controls (createButton "line" (modeSet :line state)))
-  (.add controls (createButton "none" (modeSet :none state)))
+  (.add controls (createButton "drag" (modeSet :drag state)))
 
   (.add frame world)
   (.add frame controls)
