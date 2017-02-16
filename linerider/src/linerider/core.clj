79fe819e689b6c149b95afbf07666dff147772e8
JJWill0ughby <willoughby0114@gmail.com>
2017-02-16 00:06:51 -0500

    Made headway on game physics. Game spawns rider object, and on play, the game applies physics to the rider causing the rider to fall due to gravity. When the rider collides with a line, it prints that a collision has occured. Next step is to be able to update the rider's velocities base on the line it collides with. This part is proved difficult thus far, but I feel like I'm close. After this is accomplished, I'll add the ability to jump to the rider, which should be pretty easy. After that, the core features of the game will be done. I will then try and add some extra features time permit, like the green flag that specifies the start of the rider, and the ability to scroll the game screen while running.

--- linerider/src/linerider/core.clj
+++ linerider/src/linerider/core.clj
@@ -1,3 +1,10 @@
+; Author1: Benjamin Efron
+; Author2: Justin Willoughby
+; Date: Winter 2016/17
+; Course: CSSE403 - Programming Language Paradigms
+;
+; Module Description: Main runner for our linerider project
+
 (ns linerider.core
   (:require linerider.gui))
 
